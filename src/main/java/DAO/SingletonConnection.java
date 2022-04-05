65b159213b2539c52e70b67778a04832ab5e0f11
Canh.Nguyen <leagueyasuo186@gmail.com>
2022-04-05 17:05:40 +0700

    canhpull

--- src/main/java/DAO/SingletonConnection.java
+++ src/main/java/DAO/SingletonConnection.java
@@ -1,5 +1,34 @@
 package DAO;
 
-public class SingletonConnection {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class SingletonConnection {
+    private SingletonConnection() {
+    }
+
+    private static Connection getConnection(){
+    Connection connection = null;
+    try {
+        Class.forName("com.mysql.jdbc.Driver");
+        connection =  DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/trua_nay_an_gi",
+                "root",
+                "2512"
+        );
+        System.out.println("thanh cong");
+    } catch (ClassNotFoundException e) {
+        System.out.println("that bai");
+        e.printStackTrace();
+    } catch (SQLException e) {
+        System.out.println("that bai");
+        e.printStackTrace();
+    }
+    return connection;
+}
+
+//    public static void main(String[] args) {
+//        getConnection();
+//    }
 }
