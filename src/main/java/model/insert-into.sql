4198d0039dd81942f0f12d2e12399ef337fafde4
Nguyenphangiang <phancruel@gmail.com>
2022-04-06 12:03:29 +0700

    fix database 2

--- src/main/java/model/insert-into.sql
+++ src/main/java/model/insert-into.sql
@@ -15,10 +15,10 @@ insert into the( tagName, luot_them, luot_xem)
 values ('bun',0,0),('grill',0,0),('pho',0,0),('com',0,0),('banh my',0,0),('my',0,0),
        ('do uong',0,0),('trang mieng',0,0),('pizza',0,0),('burger',0,0),('japan',0,0),
        ('korean',0,0),('thailand',0,0),('italy',0,0),('hongkong',0,0),('fries',0,0);
-insert into mon_an(name, image, note, price, khuyen_mai_id, nha_hang_id)
-values ('bun hai san','jpg/bun-hai-san.jpg','su ket hop cua tom,muc,be be tao nen huong vi kho quen',60000,1,1),
-       ('my xao hai san','jpg/my-xao-hai-san','sot hai san voi soi my dai dai, lua chon hoan hao cua ban',55000,1,1),
-       ('com hai san','jpg/com-hai-san.jpg','hat com mem duoc dung voi tom,muc,so diep goi nho ve tai nguyen bien',55000,1,1),
-       ('nuoc ep tao','jpg/nuoc-ep-tao.jpg','su chon loc ky luong tu nhung qua tao ngon nhat',10000,1,1),
-       ('tra dao','jpg/tra-dao.jpg','huong vi kho quen den tu nhung qua dao chin mong',12000,1,1),
-       ('my udon','jpg/my-udon.jpg','soi my to cung vs nuoc dung katsuo cuc ky thom ngon bo duong',40000,)
+insert into mon_an(name, image, note, price, nha_hang_id)
+values ('bun hai san','jpg/bun-hai-san.jpg','su ket hop cua tom,muc,be be tao nen huong vi kho quen',60000,1),
+       ('my xao hai san','jpg/my-xao-hai-san','sot hai san voi soi my dai dai, lua chon hoan hao cua ban',55000,1),
+       ('com hai san','jpg/com-hai-san.jpg','hat com mem duoc dung voi tom,muc,so diep goi nho ve tai nguyen bien',55000,1),
+       ('nuoc ep tao','jpg/nuoc-ep-tao.jpg','su chon loc ky luong tu nhung qua tao ngon nhat',10000,1),
+       ('tra dao','jpg/tra-dao.jpg','huong vi kho quen den tu nhung qua dao chin mong',12000,1),
+       ('my udon','jpg/my-udon.jpg','soi my to cung vs nuoc dung katsuo cuc ky thom ngon bo duong',40000,1),
